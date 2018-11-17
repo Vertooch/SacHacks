192a1964c2fab9ff64920a503a98e53954c4707e
John Verticchio <>
2018-11-17 11:16:42 -0800

    Basic skeleton for customization scene

---
+++ Assets/_Scripts/CustomPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomPlayer : MonoBehaviour {
+
+    public SpriteRenderer head;
+    public Sprite[] heads;
+
+    private int headIndex = 0;
+    
+
+    public void SwapHeadRight()
+    {
+        Debug.Log("swap head right");
+        headIndex++;
+        head.sprite = heads[headIndex];
+    }
+
+    public void SwapHeadLeft()
+    {
+        Debug.Log("swap head left");
+        headIndex--;
+        head.sprite = heads[headIndex];
+    }
+
+}
