98b1da9d5a58f249462927b95398e1843d77aa86
John Verticchio <>
2018-11-17 11:36:06 -0800

    Initial setup for leaderboard scene

---
+++ Assets/_Scripts/Leaderboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour {
+
+    public GameObject scoreEntry;
+    public GameObject scrollContent;
+    public int numScores;
+
+    // Use this for initialization
+    void Start () {
+
+        for (int i = 0; i < numScores; i++)
+        {
+            GameObject score = Instantiate(scoreEntry, scrollContent.transform, false) as GameObject;
+            score.GetComponent<Text>().text = "Score " + (i + 1);
+        }
+	}
+	
+}
