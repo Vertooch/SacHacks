4ac1206349dece91e709d95f124520e669d5b39d
jaebooker <jaesonbooker@gmail.com>
2018-11-18 00:26:56 -0800

    label

---
+++ Assets/_Scripts/alertScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class alertScript : MonoBehaviour {
+    public Text _alertText;
+    // Use this for initialization
+    void Start()
+    {
+        _alertText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _alertText.text = ";
+    }
+}
